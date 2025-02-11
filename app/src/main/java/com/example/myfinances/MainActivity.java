be48d1b76862ccc366d442cd1a768a4d003e098a
Avb2 <bringuel.alexander@gmail.com>
2025-02-11 18:59:14 -0500

    Add radio button checked logic

--- app/src/main/java/com/example/myfinances/MainActivity.java
+++ app/src/main/java/com/example/myfinances/MainActivity.java
@@ -18,10 +18,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        initState();
+        radioGroupOnChange();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    void initState(){
+        RadioButton cdButton = findViewById(R.id.cdRadio);
+        EditText paymentAmountField = findViewById(R.id.paymentAmountEditText);
+        cdButton.setChecked(true);
+        paymentAmountField.setEnabled(false);
+    }
+
+    void radioGroupOnChange(){
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                /// Radio Buttons
+                RadioButton cdButton = findViewById(R.id.cdRadio);
+                RadioButton loanButton = findViewById(R.id.loanRadio);
+                RadioButton checkingButton = findViewById(R.id.checkingRadio);
+
+                /// Text Fields
+                EditText accountNumberField = findViewById(R.id.accountNumberEditText);
+                EditText initialBalanceField = findViewById(R.id.initialBalanceEditText);
+                EditText currentBalanceField = findViewById(R.id.currentBalanceEditText);
+                EditText interestRateField = findViewById(R.id.interestRateEditText);
+                EditText paymentAmountField = findViewById(R.id.paymentAmountEditText);
+
+
+                if (cdButton.isChecked()){
+                    accountNumberField.setEnabled(true);
+                    initialBalanceField.setEnabled(true);
+                    currentBalanceField.setEnabled(true);
+                    interestRateField.setEnabled(true);
+
+                    paymentAmountField.setEnabled(false);
+                } else if (loanButton.isChecked()) {
+                    accountNumberField.setEnabled(true);
+                    initialBalanceField.setEnabled(true);
+                    currentBalanceField.setEnabled(true);
+                    interestRateField.setEnabled(true);
+                    paymentAmountField.setEnabled(true);
+                } else if (checkingButton.isChecked()) {
+                    accountNumberField.setEnabled(true);
+                    initialBalanceField.setEnabled(false);
+                    currentBalanceField.setEnabled(true);
+                    interestRateField.setEnabled(false);
+                    paymentAmountField.setEnabled(false);
+                }
+
+            }
+
+
         });
     }
 
